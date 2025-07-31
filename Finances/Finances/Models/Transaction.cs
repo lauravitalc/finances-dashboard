3d6089247926ce3229901b86936996ea5e940dc6
lauravitalc <laura_vitalc@hotmail.com>
2025-07-30 21:09:08 -0300

    feat: add Transaction model and remove WeatherForecast class

---
+++ Finances/Finances/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Finances
+{
+    public class Transaction
+    {
+
+        public int Id { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string? Category { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
