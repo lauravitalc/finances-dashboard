8b20b94faafb2e75d4d2725fc724483ed930389f
lauravitalc <laura_vitalc@hotmail.com>
2025-10-06 20:14:04 -0300

    feat: add axios dependency and update Button component to accept button type

--- finances-front/src/components/ui/Button.tsx
+++ finances-front/src/components/ui/Button.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 interface ButtonProps {
     className?: string,
+    type?: "button" | "submit" | "reset",
     children: React.ReactNode,
     onClick?: () => void
 }
 
-const Button: React.FC<ButtonProps> = ({ className, children, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ className, type, children, onClick }) => {
     return (
-        <button className={`btn ${className}`} onClick={onClick}>
+        <button className={`btn ${className}`} onClick={onClick} type={type}>
             {children}
         </button>
     )
