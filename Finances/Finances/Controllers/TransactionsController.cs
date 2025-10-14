5e34b519f9545b89870b64bba7da6a387d9adf6f
lauravitalc <laura_vitalc@hotmail.com>
2025-10-14 20:08:39 -0300

    feat: transactions controller and repository

---
+++ Finances/Finances/Controllers/TransactionsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finances.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetTransactions()
+        {
+            // Logic to get transactions
+            return Ok(new[] { "Transaction1", "Transaction2" });
+        }
+
+        [HttpGet]
+        public IActionResult GetTransactions(int id) {
+            // Logic to get a specific transaction by id
+            return Ok($"Transaction {id}");
+        }
+
+        [HttpPost]
+        public IActionResult CreateTransaction()
+        {
+            // Logic to create a transaction
+            return Ok("Transaction created");
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult UpdateTransaction(int id)
+        {
+            // Logic to update a transaction
+            return Ok($"Transaction {id} updated");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTransaction(int id)
+        {
+            // Logic to delete a transaction
+            return Ok($"Transaction {id} deleted");
+        }
+    }
+}
