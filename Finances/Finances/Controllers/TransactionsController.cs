6b471efc471861bc2ba00318f66d72a305162870
lauravitalc <laura_vitalc@hotmail.com>
2025-10-28 18:42:38 -0300

    refactor: remove unused using directive and clean up transaction methods in TransactionsController

--- Finances/Finances/Controllers/TransactionsController.cs
+++ Finances/Finances/Controllers/TransactionsController.cs
@@ -1,6 +1,5 @@
 ﻿using Finances.Interfaces.Services;
 using Finances.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Finances.Controllers
@@ -18,34 +17,31 @@ namespace Finances.Controllers
         [HttpGet]
         public IActionResult GetTransactions()
         {
-            // Logic to get transactions
             return Ok(new[] { "Transaction1", "Transaction2" });
         }
 
-        [HttpGet]
-        public IActionResult GetTransactionById(int id) {
-            // Logic to get a specific transaction by id
+        [HttpGet("{id:guid}")]
+        public IActionResult GetTransactionById(Guid id)
+        {
             return Ok($"Transaction {id}");
         }
 
         [HttpPost]
         public IActionResult CreateTransaction(Transaction transaction)
         {
-           _transactionsService.CreateAsync(transaction);
+            _transactionsService.CreateAsync(transaction);
             return CreatedAtAction(nameof(GetTransactionById), new { id = transaction.Id }, transaction);
         }
 
         [HttpPatch("{id}")]
         public IActionResult UpdateTransaction(int id)
         {
-            // Logic to update a transaction
             return Ok($"Transaction {id} updated");
         }
 
         [HttpDelete("{id}")]
         public IActionResult DeleteTransaction(int id)
         {
-            // Logic to delete a transaction
             return Ok($"Transaction {id} deleted");
         }
     }
